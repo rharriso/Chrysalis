869fa58d1f551519c3f891213aeaa81b22303042
Ross Harrison <rtharrison86@gmail.com>
2015-11-19 03:58:46 -0500

    added ground and tracking camera

---
+++ Assets/CameraBounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBounds : MonoBehaviour {
+    static Bounds get()
+    {
+        return current.bounds;
+    } 
+
+    static CameraBounds current; 
+    Bounds bounds;
+
+	// Use this for initialization
+	void Start () {
+        current = this;
+         var vertExtent = Camera.main.GetComponent<Camera>().orthographicSize;    
+         var horzExtent = vertExtent * Screen.width / Screen.height;
+ 
+        bounds = new Bounds(transform.position,
+            new Vector2(horzExtent, vertExtent));
+	}
+
+    void Update()
+    {
+        bounds.center = transform.position;
+        Debug.Log(bounds);
+    }
+}
---
+++ Assets/Ground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+    [SerializeField]
+    GameObject groundCell;
+
+    GameObject[] groundCells;
+    Bounds initBounds;
+
+	// Use this for initialization
+	void Start () {
+        // create three ground cells
+        initBounds = groundCell.GetComponent<BoxCollider2D>().bounds;
+        Debug.Log(initBounds.size.x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
